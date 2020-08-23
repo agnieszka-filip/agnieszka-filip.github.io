af09a1bdd5669b7162105565cb487b319e43626e
agnieszka-filip <agnieszka.fi94@gmail.com>
2020-08-23 23:36:33 +0200

    Install Ng-Bootstrap and create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { AboutMeComponent } from './main/about-me/about-me.component';
+import { ProjectsComponent } from './main/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    MainComponent,
+    AboutMeComponent,
+    ProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
